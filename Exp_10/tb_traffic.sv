1266ac36dd58bdab795b6e1bde10fd1d27f5c859
NehaCT704 <nehact.ec23@bmsce.ac.in>
2026-02-04 09:36:45 +0530

    Create tb_traffic.sv

---
+++ Exp_10/tb_traffic.sv
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------------------
+// File        : tb_traffic.sv
+// Author      : Neha C T / 1BM23EC164
+// Created     : 2026-02-04
+// Module      : tb_traffic
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : Simple testbench for Traffic Light Controller. Randomizes inputs and uses a 
+//               covergroup to measure input combination coverage..
+// ----------------------------------------------------------------------------------
