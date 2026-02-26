7f5d68f9f0bb1bae8fc23dc6dc81482b7d46bb22
NehaCT704 <nehact.ec23@bmsce.ac.in>
2026-02-26 23:29:24 +0530

    Update clock_interface.sv

--- AAT_2/clock_interface.sv
+++ AAT_2/clock_interface.sv
@@ -1,3 +1,13 @@
+// ----------------------------------------------------------------------------------
+// File        : clock_interface.sv
+// Author      : Neha C T / 1BM23EC164
+// Created     : 2026-02-26
+// Module      : clock_if
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : Digital Clock interface designed to connect design and tb.
+// ----------------------------------------------------------------------------------
 interface clock_if;
     logic clk;
     logic reset;
